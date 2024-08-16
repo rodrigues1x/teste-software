56fbef8a160f270a90d1ada63d4ca0bf5f547373
rodrigues1x <lorenzomeni77@gmail.com>
2024-08-16 15:59:37 -0300

    comando peersonalizado criado.

--- testes-e2e-cypress-main/cypress/e2e/spec.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe('Pagina inicial', () => {
     cy.visit('http://localhost:3000/')
   })
   it('Deve renderizar h1 com o texto correto!', () => {
-    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.getByData("principal").find("h1").should("contain", "Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!")
     cy.getByData('titulo-sub').contains('Vantagens do nosso banco:')
   })
   it('Deve renderizar h2 com o texto correto!', () => {
