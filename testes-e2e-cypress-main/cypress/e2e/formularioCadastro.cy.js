64303b1e76570e7d0627eebdda0f732af233f9b3
rodrigues1x <lorenzomeni77@gmail.com>
2024-08-29 09:07:02 -0300

    navegação pagina de cartões realizada com sucesso

--- testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -2,10 +2,11 @@ describe('Formulário cadastro', () => {
     beforeEach(()=> {
     cy.visit('http://localhost:3000')
     })
+    
     it('Usuário deve conseguir se cadastrar com sucesso', () => {
     cy.getByData('botao-cadastro').click() /* testaremos agora o botão 'cadastrar' */
     cy.getByData('nome-input').type('Jose da Silva')
-    cy.getByData('email-input').type('z@email.com')
+    cy.getByData('email-input').type('zoooo@email.com')
     cy.getByData('senha-input').type('456789')
     cy.getByData('botao-enviar').click()
     cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
@@ -16,3 +17,5 @@ describe('Formulário cadastro', () => {
     será realizado com sucesso. */
     })
     })
+
+    
