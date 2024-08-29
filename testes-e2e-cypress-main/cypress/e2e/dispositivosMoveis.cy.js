0e1eae4c2fceac44e3a2404a8b3a03bdc0be98ae
rodrigues1x <lorenzomeni77@gmail.com>
2024-08-29 11:34:03 -0300

    pagina de dispositivos moveis criada e funcionando, com exeção do 720p que tem que dar erro

--- testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
@@ -1,6 +1,5 @@
 describe('Testando dispositivos móveis', () => {
     beforeEach(() => {
-        /* roda os testes como se fossem em um dispositivo com a resolução de um iphone-5 */
         cy.viewport('iphone-5')
     })
     it('Deve existir um menu burguer', () => {
@@ -20,3 +19,49 @@ describe('Testando dispositivos móveis', () => {
     })
 })
 
+describe('Menu de navegação', () => {
+    context('Resolução de 720p', () => {
+      beforeEach(() => {
+        cy.viewport(1280, 720)
+    })
+    it('Deve existir um menu burguer', () => {
+        cy.visit('/')
+        
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        
+        cy.location('pathname').should('eq','/home')
+
+        cy.getByData('menu-burguer').click()
+        cy.getByData('menu-lateral').find('a').eq(3).click()			
+
+        cy.location('pathname').should('eq','/home/investimentos')			
+    })
+    
+     
+      
+    })
+
+    context('Resolução do iphone-5 ', () => {
+      beforeEach(() => {
+        cy.viewport('iphone-5')
+      })
+      it('Deve existir um menu burguer', () => {
+        cy.visit('/')
+        
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        
+        cy.location('pathname').should('eq','/home')
+
+        cy.getByData('menu-burguer').click()
+        cy.getByData('menu-lateral').find('a').eq(3).click()			
+
+        cy.location('pathname').should('eq','/home/investimentos')			
+    })
+    })
+  })
